6d2d5f2521d0a64a5912271e588eb45d17389727
Matheus Vieira <matheusvieira0101@gmail.com>
2024-08-08 23:31:18 -0300

    feat: create getPokemonDetail function

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,10 +1,17 @@
 
 const pokeApi = {}
 
-pokeApi.getPokemons = function(offset = 0, limit = 10) {
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url).then((response) => response.json())
+}
+
+pokeApi.getPokemons = function(offset = 0, limit = 5) {
     const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
     return fetch(url)
             .then((response) => response.json())
             .then((responseBody) => responseBody.results)
+            .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+            .then((detailRequests) => Promise.all(detailRequests)) //Wait all Pokemon Details to be fetched
+            .then((pokemonsDetails) => pokemonsDetails)
             .catch((error) => console.error(error))
 }
